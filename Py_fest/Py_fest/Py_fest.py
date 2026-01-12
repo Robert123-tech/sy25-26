d5cbcf8fda8dd3a5b674618b3596aef7549946a8
Robert!learner <rrgoo145@learner.dvusd.org>
2026-01-12 14:02:16 -0700

    cool

--- Py_fest/Py_fest/Py_fest.py
+++ Py_fest/Py_fest/Py_fest.py
@@ -1,1 +1,16 @@
+print("---Py-Fest 2026 stage manager!---")
+print("1.view lineup & Total time")
+print("2.add new band ")
+print("3.move first band to the end")
+print("4.remove band by name")
+print("5.move band to a specific position")
+print("6.exit")
+
+line_up = [
+    ("code Play", "indie", 30),
+    ("the pythonistas", "rock", 45),
+    ("syntax erorr", "Metal", 60),
+    ]
+while True:
+  choice = input("select an option (1-6) ")
 
